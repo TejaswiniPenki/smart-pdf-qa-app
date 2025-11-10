c79b933c13c7eaa6cd4cbd93ddefbcf913ca330e
TejaswiniPenki <tejaswinipenki@gmail.com>
2025-11-10 17:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader, Uns
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
-from langchain_community.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chains.combine_documents import create_stuff_documents_chain
 from langchain.docstore.document import Document
+
 
 
 # ---------- Streamlit Setup ----------
